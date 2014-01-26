d6e53f760018e79e5fd038e4df7640b9dbbd7031
Mark Conroy <markconroy@Marks-MacBook-Pro-2.local>
2014-01-26 20:54:07 +0000

    adding first commit of index and CSS files

--- index.php
+++ index.php
@@ -1,14 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta description="Portumna Cycling Club - a small, friendly cycling club based in Portumna, Galway."
   <title>Welcome to Portumna Cycling Club - cycling in Galway, Ireland</title>
+  <meta description="Portumna Cycling Club - a small, friendly cycling club based in Portumna, Galway.">
+  <link rel="stylesheet" href="stylesheets/screen.css">
 </head>
 <body>
   <div id-"page-wrapper">
     <div id="main-wrapper">
-      <p>This is some text</p>
-
+      <div id="header">
+        <p>Portumna Cycling Club</p>
+      </div>
+      <div id="menu">
+        <ul>
+          <li class="menu-item"><a href="/">Home</a></li>
+          <li class="menu-item"><a href="/">About Us</a></li>
+          <li class="menu-item"><a href="/">Contact Us</a></li>
+      </div>
+      <div id="content">
+      </div>
+      <div id="footer">
+      </div>
     </div>
   </div>
 </body>
