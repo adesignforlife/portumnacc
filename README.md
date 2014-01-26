84722776f98c51bce62de2a92698aec36882ae89
Mark Conroy <markconroy@Marks-MacBook-Pro-2.local>
2014-01-26 20:19:32 +0000

    first commit of basic setup

---
+++ README.md
@@ -0,0 +1,6 @@
+Portumna Cycling Club - site code
+==================
+
+A repository for the Portumna Cycling Club code. Created by Mark Conroy - www.mark.ie
+
+If you find any of this useful, you are welcome to use it.
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta description="Portumna Cycling Club - a small, friendly cycling club based in Portumna, Galway."
+  <title>Welcome to Portumna Cycling Club - cycling in Galway, Ireland</title>
+</head>
+<body>
+  <div id-"page-wrapper">
+    <div id="main-wrapper">
+      <p>This is some text</p>
+
+    </div>
+  </div>
+</body>
+</html>
