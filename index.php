8af4cd2dc57f1f78c0361ba8507c6f887dbb32d1
Mark Conroy <markconroy@Marks-MacBook-Pro-2.local>
2014-01-27 21:19:30 +0000

    adding zen-grids support

--- index.php
+++ index.php
@@ -14,13 +14,44 @@
       <div id="menu">
         <ul>
           <li class="menu-item"><a href="/">Home</a></li>
-          <li class="menu-item"><a href="/">About Us</a></li>
-          <li class="menu-item"><a href="/">Contact Us</a></li>
+          <li class="menu-item"><a href="#about-us">About Us</a></li>
+          <li class="menu-item"><a href="#contact-us">Contact Us</a></li>
       </div>
-      <div id="content">
+      <div id ="slider">
+        <p>Let's put a jQuery slider in here.</p>
+      </div>
+      <div id="main">
+        <div class="main-content">
+           <a name="about-us"></a>
+          <h2>Welcome to Portumna Cycling Club</h2>
+          <p>We are Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>
+          <p>Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus.</p>
+          <p>Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem.</p>
+        </div>
+        <div class="main-aside">
+          <h2>When We Meet</h2>
+          <p> Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus.</p>
+        </div>
+        <div class="main-content">
+        <a name="contact-us"></a>
+          <h2>Committee and Contacts</h2>
+          <p>We are Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>
+        </div>
+        <div class="main-aside">
+          <h2>Join the Club</h2>
+          <p> Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus.</p>
+        </div>
       </div>
       <div id="footer">
-        <p>This site is under construction - as you can see.</p>
+        <div class="footer-block-1">
+          <p>This site was created by <a href="http://www.mark.ie">Mark Conroy</a></p>
+        </div>
+        <div class="footer-block-2">
+          <p>This site was created by <a href="http://www.mark.ie">Mark Conroy</a></p>
+        </div>
+        <div class="footer-block-3">
+          <p>This site was created by <a href="http://www.mark.ie">Mark Conroy</a></p>
+        </div>
       </div>
     </div>
   </div>
