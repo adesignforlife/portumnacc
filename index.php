e752a43f0ec6658f727765624d32799394cdc4bc
Mark Conroy <markconroy@Marks-MacBook-Pro-2.local>
2014-01-30 15:43:25 +0000

    adding smooth scroll

--- index.php
+++ index.php
@@ -16,9 +16,9 @@
         <div id="menu-wrapper-inner">
           <div id="menu">
             <ul>
-              <li class="menu-item-1"><a href="/">Home</a></li>
-              <li class="menu-item-2"><a href="#about-us">About Us</a></li>
-              <li class="menu-item-3"><a href="#contact-us">Contact Us</a></li>
+              <li class="menu-item-1"><a href="index.php">Home</a></li>
+              <li class="menu-item-2"><a data-scroll data-speed="500" data-easing="easeInOutCubic" data-url="false" href="#about-us">About Us</a></li>
+              <li class="menu-item-3"><a data-scroll data-speed="500" data-easing="easeInOutCubic" data-url="false" href="#contact-us">Contact Us</a></li>
           </div>
         </div>
       </div>
@@ -28,7 +28,7 @@
       </div>
       <div id="main">
         <div class="main-content">
-           <a name="about-us"></a>
+           <span id="about-us"></span>
           <h2>Welcome to Portumna Cycling Club</h2>
           <p>We are Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>
           <p>Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus.</p>
@@ -39,7 +39,7 @@
           <p> Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus.</p>
         </div>
         <div class="main-content">
-        <a name="contact-us"></a>
+        <span id="contact-us"></span>
           <h2>Committee and Contacts</h2>
           <p>We are Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>
         </div>
@@ -51,6 +51,7 @@
     </div>
     <div id="footer-wrapper">
       <div id="footer">
+        <script src="smooth-scroll.js"></script>
         <div class="footer-block-1">
           <p>This site was created by <a href="http://www.mark.ie">Mark Conroy</a></p>
         </div>
